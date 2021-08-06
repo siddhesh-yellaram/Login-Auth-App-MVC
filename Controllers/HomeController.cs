f94107a8343f78d89f5314506043e8f3be8a803f
Siddhesh Yellaram <58585975+siddhesh-yellaram@users.noreply.github.com>
2021-08-07 02:26:58 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginAuthApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        //GET: Auth
+        public ActionResult Auth()
+        {
+            return View();
+        }
+    }
+}
